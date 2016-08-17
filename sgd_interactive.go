203345dc088ef98ba85e4003ce5400fe63cbaebf
Alex Nichol <unixpickle@gmail.com>
2016-08-17 14:49:31 -0400

    specify that sample set modification is allowed

--- sgd_interactive.go
+++ sgd_interactive.go
@@ -13,6 +13,9 @@ import (
 // function before every epoch and stops when
 // said function returns false, or when the
 // user sends a kill signal.
+// The sf function may modify the SampleSet,
+// and the new sample set will be used on the
+// next iteration.
 func SGDInteractive(g Gradienter, s SampleSet, stepSize float64, batchSize int, sf func() bool) {
 	var killed uint32
 
