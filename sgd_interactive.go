279b2614079e81fcf5601d6523b4b708d67f1145
Alex Nichol <unixpickle@gmail.com>
2016-09-09 18:44:05 -0400

    added mini-batch-based interactive training

--- sgd_interactive.go
+++ sgd_interactive.go
@@ -2,6 +2,8 @@
 
 package sgd
 
+import "github.com/unixpickle/sgd"
+
 // SGDInteractive is like SGD, but it calls sf before
 // each epoch and stops when sf returns false.
 //
@@ -14,5 +16,34 @@ package sgd
 func SGDInteractive(g Gradienter, s SampleSet, stepSize float64, batchSize int, sf func() bool) {
 	loopUntilKilled(sf, func() {
 		SGD(g, s, stepSize, 1, batchSize)
+	})
+}
+
+// SGDMini is like SGDInteractive, but the interactive
+// function is called for each mini-batch rather than
+// for each complete epoch.
+// Since each mini-batch is different, sf is passed the
+// next mini-batch so it can perform mini-batch-specific
+// tasks.
+func SGDMini(g Gradienter, s SampleSet, stepSize float64, batchSize int,
+	sf func(batch SampleSet) bool) {
+	shuffledSet := s.Copy()
+	sampleIdx := shuffledSet.Len()
+	var subset sgd.SampleSet
+	loopUntilKilled(func() bool {
+		sampleIdx += batchSize
+		if sampleIdx >= shuffledSet.Len() {
+			sampleIdx = 0
+			sgd.ShuffleSampleSet(shuffledSet)
+		}
+		bs := batchSize
+		if bs > shuffledSet.Len()-sampleIdx {
+			bs = shuffledSet.Len() - sampleIdx
+		}
+		subset = shuffledSet.Subset(sampleIdx, bs)
+		return sf(subset)
+	}, func() {
+		grad := g.Gradient(subset)
+		grad.AddToVars(-stepSize)
 	})
 }
